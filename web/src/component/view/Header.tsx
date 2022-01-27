28bbfcfd2279374df0a9dbe3ab09143378b7483f
Andrei Sergeev <aelsergeev@gmail.com>
2022-01-27 23:47:07 +0300

    change header side texts ui
    (cherry picked from commit 910a4b783eae5d93da8345e9a7dac0ada964d43b)

--- web/src/component/view/Header.tsx
+++ web/src/component/view/Header.tsx
@@ -4,10 +4,12 @@ import React from "react";
 import {useTheme} from "../../provider/ThemeProvider";
 
 const SX = {
-    titleText: { fontSize: '35px', fontWeight: 900, fontFamily: 'monospace' },
-    sidesText: { fontSize: '20px', padding: '0px 20px', fontWeight: 900 },
-    captionText: { fontSize: '12px', fontWeight: 500, fontFamily: 'monospace' },
+    title: { fontSize: '35px', fontWeight: 900, fontFamily: 'monospace' },
+    sides: { fontSize: '20px', margin: '5px 20px', height: '100%', fontWeight: 900, borderRadius: '15px' },
+    caption: { fontSize: '12px', fontWeight: 500, fontFamily: 'monospace' },
     container: { marginBottom: '10px' },
+    emblem: { padding: '5px 15px' },
+    buttons: { padding: '0px 5px' }
 }
 
 export function Header() {
@@ -16,15 +18,19 @@ export function Header() {
 
     return (
         <Grid container sx={SX.container} justifyContent="space-between" alignItems="center">
-            <Grid item sx={SX.sidesText}>VEEGRES</Grid>
-            <Grid item textAlign={"center"}>
-                <Box sx={{ ...SX.titleText, color }}>Ivory</Box>
-                <Box sx={SX.captionText}>[postgres cluster visualization]</Box>
+            <Grid item sx={{ ...SX.sides, borderLeft: `1px solid ${color}` }}>
+                <Box sx={SX.emblem}>VEEGRES</Box>
             </Grid>
-            <Grid item>
-                <IconButton onClick={theme.toggle}>
-                    {theme.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
-                </IconButton>
+            <Grid item textAlign={"center"}>
+                <Box sx={{ ...SX.title, color }}>Ivory</Box>
+                <Box sx={SX.caption}>[postgres cluster visualization]</Box>
+            </Grid>
+            <Grid item sx={{ ...SX.sides, borderRight: `1px solid ${color}` }}>
+                <Box sx={SX.buttons}>
+                    <IconButton onClick={theme.toggle}>
+                        {theme.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
+                    </IconButton>
+                </Box>
             </Grid>
         </Grid>
     )
