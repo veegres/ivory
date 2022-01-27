31c08267a4a7f1d736112d1e5599e62bb920bbf7
Andrei Sergeev <aelsergeev@gmail.com>
2022-01-27 23:47:02 +0300

    update Header.tsx
    (cherry picked from commit aac7d1411bfa36e8e24624c8e93d4599344ff1dd)

--- web/src/component/view/Header.tsx
+++ web/src/component/view/Header.tsx
@@ -1,11 +1,13 @@
-import {Grid, IconButton} from "@mui/material";
+import {Box, Grid, IconButton} from "@mui/material";
 import {Brightness4, Brightness7} from "@mui/icons-material";
 import React from "react";
 import {useTheme} from "../../provider/ThemeProvider";
 
 const SX = {
-    titleText: { fontSize: '30px', fontWeight: 900 },
-    sidesText: { fontSize: '20px', padding: '0px 20px', fontWeight: 900 }
+    titleText: { fontSize: '35px', fontWeight: 900, fontFamily: 'monospace' },
+    sidesText: { fontSize: '20px', padding: '0px 20px', fontWeight: 900 },
+    captionText: { fontSize: '12px', fontWeight: 500, fontFamily: 'monospace' },
+    container: { marginBottom: '10px' },
 }
 
 export function Header() {
@@ -13,9 +15,12 @@ export function Header() {
     const color = theme.info?.palette.primary.main
 
     return (
-        <Grid container justifyContent="space-between" alignItems="center">
+        <Grid container sx={SX.container} justifyContent="space-between" alignItems="center">
             <Grid item sx={SX.sidesText}>VEEGRES</Grid>
-            <Grid item sx={{ ...SX.titleText, color }}>POSTGRES CLUSTER GUI</Grid>
+            <Grid item textAlign={"center"}>
+                <Box sx={{ ...SX.titleText, color }}>Ivory</Box>
+                <Box sx={SX.captionText}>[postgres cluster visualization]</Box>
+            </Grid>
             <Grid item>
                 <IconButton onClick={theme.toggle}>
                     {theme.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
