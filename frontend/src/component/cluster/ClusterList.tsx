91da9e276fbb14e57f1893a21615b34f1faeb973
Andrei Sergeev <aelsergeev@gmail.com>
2022-01-27 23:41:55 +0300

    add readonly table
    (cherry picked from commit 231c4f575b1ee91e4000aa5eceb6b82ade67aa08)

--- frontend/src/component/cluster/ClusterList.tsx
+++ frontend/src/component/cluster/ClusterList.tsx
@@ -2,13 +2,14 @@ import {Table, TableCell, TableHead, TableRow} from "@mui/material";
 import { useQuery } from "react-query";
 import {clusterApi} from "../../app/api";
 import {ClusterListRow} from "./ClusterListRow";
-import React from "react";
+import React, {useState} from "react";
 import {Error} from "../view/Error";
 import {TableBodyLoading} from "../view/TableBodyLoading";
 import {TableCellFetching} from "../view/TableCellFetching";
 import {AxiosError} from "axios";
 
 export function ClusterList() {
+    const [editNode, setEditNode] = useState('')
     const { data: clusterMap, isLoading, isFetching, isError, error } = useQuery('cluster/list', clusterApi.list)
     if (isError) return <Error error={error as AxiosError} />
 
@@ -32,10 +33,13 @@ export function ClusterList() {
 
         return (
             <>
-                {Object.entries(clusterMap).map(([name, nodes]) => (
-                    <ClusterListRow key={name} nodes={nodes} name={name} />
-                ))}
-                <ClusterListRow name={''} nodes={['']} />
+                {Object.entries(clusterMap).map(([name, nodes]) => {
+                    const isReadOnly = name !== editNode
+                    const toggleEdit = () => setEditNode(isReadOnly ? name : '')
+                    const edit = { isReadOnly, toggleEdit }
+                    return <ClusterListRow key={name} nodes={nodes} name={name} edit={edit} />
+                })}
+                <ClusterListRow name={''} nodes={['']} edit={{ isReadOnly: false, toggleEdit: () => {} }} />
             </>
         )
     }
--- frontend/src/component/cluster/ClusterListRow.tsx
+++ frontend/src/component/cluster/ClusterListRow.tsx
@@ -1,21 +1,26 @@
-import {Box, CircularProgress, FormControl, IconButton, Input, TableCell, TableRow} from "@mui/material";
-import {Add, CheckCircle, Delete, Remove, Visibility} from "@mui/icons-material";
-import {ReactNode, useState} from "react";
+import {Box, Chip, CircularProgress, FormControl, IconButton, OutlinedInput, TableCell, TableRow} from "@mui/material";
+import {Add, Check, Delete, Edit, Remove, Visibility} from "@mui/icons-material";
+import {cloneElement, ReactElement, useState} from "react";
 import {useMutation, useQueryClient} from "react-query";
 import {clusterApi} from "../../app/api";
 import {ClusterMap} from "../../app/types";
 import {useStore} from "../../provider/StoreProvider";
 
 const SX = {
-    clusterNameIcon: { fontSize: 10 },
-    tableIcon: { fontSize: 14 },
-    input: { minWidth: '150px' }
+    clusterCellIcon: { fontSize: 10 },
+    actionCellIcon: { fontSize: 18 },
+    actionCellButton: { height: '32px', width: '32px' },
+    nodesCellIcon: { fontSize: 18 },
+    nodesCellButton: { height: '22px', width: '22px' },
+    nodesCellBox: { minWidth: '150px' },
+    nodesCellInput: { height: '32px' }
 }
 
-export function ClusterListRow(props: { nodes: string[], name: string }) {
+export function ClusterListRow(globalProps: { name: string, nodes: string[], edit: { isReadOnly: boolean, toggleEdit: () => void } }) {
     const { store, setStore } = useStore()
-    const [name, setName] = useState(props.name);
-    const [nodes, setNodes] = useState(props.nodes.length ? props.nodes : ['']);
+    const { isReadOnly, toggleEdit } = globalProps.edit
+    const [name, setName] = useState(globalProps.name);
+    const [nodes, setNodes] = useState(globalProps.nodes.length ? globalProps.nodes : ['']);
 
     const queryClient = useQueryClient();
     const updateCluster = useMutation(clusterApi.update, {
@@ -36,63 +41,120 @@ export function ClusterListRow(props: { nodes: string[], name: string }) {
     return (
         <TableRow>
             <TableCell sx={{ verticalAlign: "top", width: '15%' }}>
-                <Box display="flex" sx={SX.input} flexDirection="row" alignItems="center" justifyContent="space-between">
-                    <FormControl fullWidth focused>
-                        <Input size="small" placeholder="Name" value={name} readOnly={!!props.name} onChange={(event) => setName(event.target.value)}/>
-                    </FormControl>
-                    <Box display="flex" flexDirection="column" alignItems="center" justifyContent="space-between">
-                        <IconButton sx={{ padding: '2px' }} onClick={handleAdd}><Add sx={SX.clusterNameIcon} /></IconButton>
-                        <IconButton sx={{ padding: '2px' }} onClick={handleRemove}><Remove sx={SX.clusterNameIcon} /></IconButton>
-                    </Box>
-                </Box>
+                <ClusterNameCell />
             </TableCell>
             <TableCell sx={{ verticalAlign: "top" }}>
-                <Box display="grid" gridTemplateColumns={`repeat(auto-fill, minmax(${SX.input.minWidth}, 1fr))`} gap={1}>
-                    {nodes.map((node, index) => (
-                        <FormControl key={index} focused>
-                            <Input
-                                color="secondary"
-                                type="string"
-                                endAdornment={<ViewEndAdornment node={node} />}
-                                placeholder={`Node ${index}`}
-                                size="small"
-                                value={node}
-                                onChange={(event) => handleChange(index, event.target.value)}
-                            />
-                        </FormControl>
-                    ))}
-                </Box>
+                <ClusterNodesCell />
             </TableCell>
             <TableCell sx={{ verticalAlign: "top", width: '1%', whiteSpace: 'nowrap' }}>
-                <CellButton isLoading={updateCluster.isLoading} onClick={handleUpdate}>
-                    <CheckCircle sx={SX.tableIcon} />
-                </CellButton>
-                <CellButton isLoading={deleteCluster.isLoading} onClick={handleDelete}>
-                    <Delete sx={SX.tableIcon} />
-                </CellButton>
+                <ClusterActionsCell />
             </TableCell>
         </TableRow>
     )
 
-    function CellButton(props: { isLoading: boolean, onClick: () => void, children: ReactNode }) {
-        const { isLoading, children, onClick } = props;
+    function ClusterNameCell() {
+        if (globalProps.name) {
+            return <Chip sx={{ width: '100%' }} label={name} />
+        }
+
         return (
-            <IconButton disabled={isLoading || !name} onClick={onClick}>
-                {isLoading ? <CircularProgress size={14} /> : children}
+            <Box display="flex" sx={SX.nodesCellBox} flexDirection="row" alignItems="center" justifyContent="space-between">
+                <FormControl fullWidth>
+                    <OutlinedInput
+                        sx={SX.nodesCellInput}
+                        placeholder="Name"
+                        value={name}
+                        onChange={(event) => setName(event.target.value)}
+                    />
+                </FormControl>
+                <Box display="flex" flexDirection="column" alignItems="center" justifyContent="space-between">
+                    <IconButton sx={{ padding: '2px' }} onClick={handleAdd}><Add sx={SX.clusterCellIcon} /></IconButton>
+                    <IconButton sx={{ padding: '2px' }} onClick={handleRemove}><Remove sx={SX.clusterCellIcon} /></IconButton>
+                </Box>
+            </Box>
+        )
+    }
+
+    function ClusterNodesCell() {
+        return (
+            <Box display="grid" gridTemplateColumns={`repeat(auto-fill, minmax(${SX.nodesCellBox.minWidth}, 1fr))`} gap={1}>
+                {nodes.map((node, index) => (<ClusterNodesCellElement node={node} index={index} />))}
+            </Box>
+        )
+    }
+
+    function ClusterNodesCellElement(props: { node: string, index: number }) {
+        const { node, index } = props
+
+        const isNodeSelected = !!node && store.activeNode === node
+
+        if (isReadOnly) {
+            return <Chip
+                key={index}
+                sx={{ width: '100%' }}
+                label={node ? node : `Node ${index}`}
+                disabled={!node}
+                variant="outlined"
+                color={isNodeSelected ? "primary" : "default"}
+                onClick={handleSelectedNodeChange(node, isNodeSelected)}
+            />
+        }
+
+        return (
+            <FormControl key={index}>
+                <OutlinedInput
+                    sx={SX.nodesCellInput}
+                    type="string"
+                    endAdornment={<NodeSelectButton node={node} isNodeSelected={isNodeSelected} />}
+                    placeholder={`Node ${index}`}
+                    size="small"
+                    value={node}
+                    onChange={(event) => handleChange(index, event.target.value)}
+                />
+            </FormControl>
+        )
+    }
+
+    function ClusterActionsCell() {
+        return (
+            <>
+                {isReadOnly ?
+                    <CellButton icon={<Edit />} isLoading={updateCluster.isLoading} onClick={toggleEdit} /> :
+                    <CellButton icon={<Check />} isLoading={updateCluster.isLoading} onClick={handleUpdate} />
+                }
+                <CellButton icon={<Delete />} isLoading={deleteCluster.isLoading} onClick={handleDelete} />
+            </>
+        )
+    }
+
+    function CellButton(props: { isLoading: boolean, onClick: () => void, icon: ReactElement }) {
+        const { isLoading, icon, onClick } = props;
+        return (
+            <IconButton sx={SX.actionCellButton} disabled={isLoading || !name} onClick={onClick}>
+                {isLoading ?
+                    <CircularProgress size={SX.actionCellIcon.fontSize} /> :
+                    cloneElement(icon, { sx: SX.actionCellIcon })
+                }
             </IconButton>
         )
     }
 
-    function ViewEndAdornment({ node }: { node: string }) {
+    function NodeSelectButton(props: { node: string, isNodeSelected: boolean }) {
+        const { node, isNodeSelected } = props
         if (!node) return null
-        const isSameNode = store.activeNode === node
+
         return (
             <IconButton
-                color={isSameNode ? "secondary" : "default"}
-                onClick={() => setStore({ activeNode: isSameNode ? '' : node })}>
-                <Visibility sx={SX.tableIcon} />
+                sx={SX.nodesCellButton}
+                color={isNodeSelected ? "primary" : "default"}
+                onClick={handleSelectedNodeChange(node, isNodeSelected)}>
+                <Visibility sx={SX.nodesCellIcon} />
             </IconButton>
         )
+    }
+
+    function handleSelectedNodeChange(selectedNode: string, isNodeSelected: boolean) {
+        return () => setStore({ activeNode: isNodeSelected ? '' : selectedNode })
     }
 
     function handleChange(index: number, value: string) {
@@ -111,6 +173,7 @@ export function ClusterListRow(props: { nodes: string[], name: string }) {
     }
 
     function handleUpdate() {
+        toggleEdit()
         updateCluster.mutate({ name, nodes })
     }
 
