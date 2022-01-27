3c00c7f41736595e0aa769134e625ff56874843b
Andrei Sergeev <aelsergeev@gmail.com>
2022-01-27 23:47:16 +0300

    edit logos and title
    (cherry picked from commit 5646417701c40755888f673508fd073169da96f2)

---
+++ web/src/react-app-env.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="react-scripts" />
+/*
+These type declarations add support for importing resource files
+such as bmp, gif, jpeg, jpg, png, webp, and svg. That means that
+the following import will work as expected without errors:
+
+`import logo from './logo.svg';`
+*/
