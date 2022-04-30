d9992665de2f9dd08d59b52a91ab61977a473a1f
Andrei Sergeev <aelsergeev@gmail.com>
2022-04-30 15:41:45 +0200

    extract DynamicInputs.tsx

--- web/src/provider/StoreProvider.tsx
+++ web/src/provider/StoreProvider.tsx
@@ -1,19 +1,31 @@
 import {createContext, ReactNode, useContext, useState} from "react";
 
 interface StoreType {
+    activeCluster: string,
     activeNode: string
+}
+
+interface StoreTypeParam {
+    activeCluster?: string
+    activeNode?: string
 }
 
 interface StoreContextType {
     store: StoreType
-    setStore: (store: StoreType) => void
+    setStore: (store: StoreTypeParam) => void
+    isClusterActive: (name: string) => boolean
+    isNodeActive: (name: string) => boolean
 }
 
 const initialStore: StoreType = {
+    activeCluster: '',
     activeNode: '',
 }
 
-const StoreContext = createContext<StoreContextType>({ store: initialStore, setStore: () => {} })
+const StoreContext = createContext<StoreContextType>({
+    store: initialStore, setStore: () => {},
+    isClusterActive: () => false, isNodeActive: () => false,
+})
 
 export function useStore() {
     return useContext(StoreContext)
@@ -22,12 +34,12 @@ export function useStore() {
 export function StoreProvider(props: { children: ReactNode }) {
     const [state, setState] = useState(initialStore)
 
-    const setStore = (store: StoreType) => {
-        setState({ ...state, ...store })
-    }
+    const setStore = (store: StoreTypeParam) => setState({ ...state, ...store })
+    const isClusterActive = (name: string) => name === state.activeCluster
+    const isNodeActive = (name: string) => name === state.activeNode
 
     return (
-        <StoreContext.Provider value={{ store: state, setStore }}>
+        <StoreContext.Provider value={{ store: state, setStore, isClusterActive, isNodeActive }}>
             {props.children}
         </StoreContext.Provider>
     )
