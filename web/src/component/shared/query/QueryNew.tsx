8e5e441dc43144c367271fe9e9d0a5f18596b32b
Andrei Sergeev <aelsergeev@gmail.com>
2023-03-12 21:17:03 +0300

    add information blocks about queries

--- web/src/component/shared/query/QueryNew.tsx
+++ web/src/component/shared/query/QueryNew.tsx
@@ -1,4 +1,4 @@
-import {Box, Paper, TextField} from "@mui/material";
+import {Alert, Box, Paper, TextField, ToggleButton} from "@mui/material";
 import {useState} from "react";
 import {SxPropsMap} from "../../../type/common";
 import {QueryEditor} from "./QueryEditor";
@@ -8,11 +8,17 @@ import {useMutation} from "@tanstack/react-query";
 import {queryApi} from "../../../app/api";
 import {LoadingButton} from "@mui/lab";
 import {QueryType} from "../../../type/query";
+import {InfoOutlined} from "@mui/icons-material";
+import {InfoBox} from "../../view/InfoBox";
 
 const SX: SxPropsMap = {
     body: {display: "flex", flexDirection: "column", gap: 1, marginBottom: "8px", padding: "10px 15px"},
     buttons: {display: "flex", alignItems: "center", gap: 1},
+    title: {display: "flex", alignItems: "center", justifyContent: "space-between"},
+    info: {display: "flex", gap: 1},
+    toggle: {padding: "3px"},
     desc: {flexGrow: 1},
+    type: {color: "secondary.main"},
     editor: {borderRadius: "5px"},
     name: {color: "text.disabled"},
 }
@@ -24,6 +30,7 @@ type Props = {
 export function QueryNew(props: Props) {
     const {type} = props
     const {info} = useTheme()
+    const [alert, setAlert] = useState(false)
     const [name, setName] = useState("")
     const [description, setDesc] = useState("")
     const [query, setQuery] = useState("")
@@ -33,6 +40,28 @@ export function QueryNew(props: Props) {
 
     return (
         <Paper sx={SX.body} variant={"outlined"}>
+            <Box sx={SX.title}>
+                <Box><b>NEW QUERY</b></Box>
+                <Box sx={SX.info}>
+                    <InfoBox tooltip={""}><Box sx={SX.type}>{QueryType[type]}</Box></InfoBox>
+                    <ToggleButton
+                        sx={SX.toggle}
+                        value={"info"}
+                        size={"small"}
+                        selected={alert}
+                        onClick={() => setAlert(!alert)}
+                    >
+                        <InfoOutlined/>
+                    </ToggleButton>
+                </Box>
+            </Box>
+            {alert && (
+                <Alert variant={"outlined"} severity={"info"} icon={false}>
+                    Fields <i>name</i> and <i>query</i> are required for a new query. If you want to have termination
+                    and query cancellation buttons in the table you need to call postgres <i>process_id</i> as
+                    a <i>pid</i>. Example: <i>SELECT pid FROM table;</i>
+                </Alert>
+            )}
             <Box sx={SX.buttons}>
                 <TextField
                     size={"small"}
@@ -47,7 +76,12 @@ export function QueryNew(props: Props) {
                     placeholder={"Description"}
                     onChange={(e) => setDesc(e.target.value)}
                 />
-                <LoadingButton loading={create.isLoading} variant={"outlined"} onClick={handleAdd}>
+                <LoadingButton
+                    loading={create.isLoading}
+                    variant={"outlined"}
+                    onClick={handleAdd}
+                    disabled={!name || !query}
+                >
                     Add
                 </LoadingButton>
             </Box>
--- web/src/component/view/InfoBox.tsx
+++ web/src/component/view/InfoBox.tsx
@@ -4,7 +4,10 @@ import {useTheme} from "../../provider/ThemeProvider";
 import {SxPropsMap} from "../../type/common";
 
 const SX: SxPropsMap = {
-    box: { display: "flex", justifyContent: "center", alignItems: "center", height: "32px", fontSize: "0.8125rem", cursor: "pointer", minWidth: "30px" },
+    box: {
+        display: "flex", justifyContent: "center", alignItems: "center", height: "32px",
+        fontSize: "0.8125rem", cursor: "pointer", minWidth: "30px"
+    },
 }
 
 type Props = {
@@ -15,14 +18,14 @@ type Props = {
 }
 
 export function InfoBox(props: Props) {
-    const { children, tooltip, withPadding, withRadius } = props
-    const { info } = useTheme()
+    const {children, tooltip, withPadding, withRadius} = props
+    const {info} = useTheme()
     const padding = withPadding ? "3px 12px" : "3px 5px"
     const borderRadius = withRadius ? "15px" : "4px"
 
     return (
         <Tooltip title={tooltip} placement={"top"} arrow>
-            <Box sx={{ ...SX.box, padding, borderRadius, border: `1px solid ${info?.palette.divider}` }}>
+            <Box sx={{...SX.box, padding, borderRadius, border: `1px solid ${info?.palette.divider}`}}>
                 {children}
             </Box>
         </Tooltip>
