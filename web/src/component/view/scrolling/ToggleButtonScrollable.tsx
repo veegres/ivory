c6a6e02ee061ac6abcc4e6809573eec6dd394239
Andrei Sergeev <aelsergeev@gmail.com>
2023-04-12 09:24:06 +0300

    add warnings count to tags

--- web/src/component/view/scrolling/ToggleButtonScrollable.tsx
+++ web/src/component/view/scrolling/ToggleButtonScrollable.tsx
@@ -10,18 +10,19 @@ const SX: SxPropsMap = {
     arrow: {width: "30px", height: "35px", borderRadius: "5px", margin: "0 3px"},
     group: {display: "flex", flexGrow: 1, padding: "0 5px", overflow: "hidden", scrollBehavior: "smooth", gap: 1},
     all: {display: "flex", alignItems: "center", marginRight: "5px", lineHeight: "1.1"},
-    count: {display: "flex", alignItems: "center", marginLeft: "5px", lineHeight: "1.1"},
+    info: {display: "flex", alignItems: "center", marginLeft: "5px", lineHeight: "1.1"},
     element: {padding: "3px 7px", borderRadius: "3px", lineHeight: "1.1", border: "1px solid", borderColor: "divider"},
 }
 
 type Props = {
     tags: string[],
     selected?: string[],
+    warnings?: number,
     onUpdate: (tags: string[]) => void
 }
 
 export function ToggleButtonScrollable(props: Props) {
-    const {tags, onUpdate} = props
+    const {tags, warnings, onUpdate} = props
     const scrollRef = useRef<Element>(null)
     const [scrolled, elements] = useWindowScrolled(scrollRef.current, tags)
     const [selected, setSelected] = useState(new Set([ALL, ...(props.selected ?? [])]))
@@ -42,10 +43,13 @@ export function ToggleButtonScrollable(props: Props) {
             <Box ref={scrollRef} sx={SX.group}>
                 {elements.map(tag => renderButton(tag, selected.has(tag), handleClick))}
             </Box>
-            <Box sx={SX.count}>
-                <Tooltip title={"Selected"} placement={"top"}>
+            <Box sx={SX.info}>
+                <Tooltip title={"Tags Selected"} placement={"top"}>
                     <span>{renderButton(count, !isAll)}</span>
                 </Tooltip>
+            </Box>
+            <Box sx={SX.info}>
+                {renderInfo()}
             </Box>
             <TabScrollButton
                 sx={SX.arrow}
@@ -56,6 +60,26 @@ export function ToggleButtonScrollable(props: Props) {
             />
         </Box>
     )
+
+    function renderInfo() {
+        if (warnings === undefined) return
+        return (
+            <Tooltip title={"Warnings"} placement={"top"}>
+                <span>
+                    <ToggleButton
+                        sx={SX.element}
+                        color={"warning"}
+                        size={"small"}
+                        selected={warnings > 0}
+                        disabled
+                        value={warnings}
+                    >
+                        {warnings}
+                    </ToggleButton>
+                </span>
+            </Tooltip>
+        )
+    }
 
     function renderButton(tag: string, selected: boolean, onClick?: (value: string) => void) {
         return (
