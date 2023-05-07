bc404502138f31650cdfd2b198a7f3988e53e870
Andrei Sergeev <aelsergeev@gmail.com>
2023-05-07 10:07:26 +0300

    refactoring

---
+++ web/src/component/cluster/overview/OverviewAction.tsx
@@ -0,0 +1,48 @@
+import {Box, ToggleButton, ToggleButtonGroup, Tooltip} from "@mui/material";
+import {OverviewActionInfo} from "./OverviewActionInfo";
+import {InfoOutlined, Settings} from "@mui/icons-material";
+import {ActiveCluster} from "../../../type/cluster";
+import {SxPropsMap} from "../../../type/common";
+
+const SX: SxPropsMap = {
+    box: {display: "flex", alignItems: "center", gap: 1},
+    toggleButton: {padding: "3px"},
+}
+
+type Props = {
+    cluster: ActiveCluster,
+    selectInfo: boolean,
+    dissableInfo: boolean,
+    toggleInfo: () => void,
+    selectOptions: boolean,
+    toggleOptions: () => void,
+}
+
+export function OverviewAction(props: Props) {
+    const {cluster, toggleOptions, selectOptions, selectInfo, toggleInfo, dissableInfo} = props
+
+    return (
+        <Box sx={SX.box}>
+            <OverviewActionInfo cluster={cluster}/>
+            <ToggleButtonGroup size={"small"}>
+                <ToggleButton
+                    sx={SX.toggleButton}
+                    value={"settings"}
+                    selected={selectOptions}
+                    onClick={toggleOptions}
+                >
+                    <Tooltip title={"Cluster Options"} placement={"top"}><Settings/></Tooltip>
+                </ToggleButton>
+                <ToggleButton
+                    sx={SX.toggleButton}
+                    value={"info"}
+                    selected={selectInfo}
+                    disabled={dissableInfo}
+                    onClick={toggleInfo}
+                >
+                    <Tooltip title={"Tab Description"} placement={"top"}><InfoOutlined/></Tooltip>
+                </ToggleButton>
+            </ToggleButtonGroup>
+        </Box>
+    )
+}
