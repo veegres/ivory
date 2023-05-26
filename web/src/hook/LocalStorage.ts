04c628b59dcdabe580717ae5c4f5e26acbc0d9d7
Andrei Sergeev <aelsergeev@gmail.com>
2023-05-26 20:28:16 +0300

    sync browser tabs

--- web/src/hook/LocalStorage.ts
+++ web/src/hook/LocalStorage.ts
@@ -1,9 +1,41 @@
 import {Dispatch, SetStateAction, useEffect, useState} from "react";
 
-export function useLocalStorageState<T>(key: string, initialState: T | (() => T)): [T, Dispatch<SetStateAction<T>>] {
+export function useLocalStorageState<T>(key: string, initialState: T | (() => T), sync = false): [T, Dispatch<SetStateAction<T>>] {
+    const isObject = typeof initialState === "object"
     const localStorageValue = localStorage.getItem(key)
-    const initValue = localStorageValue !== null ? JSON.parse(localStorageValue) as T : initialState
-    const [value, setValue] = useState(initValue)
-    useEffect(() => localStorage.setItem(key, JSON.stringify(value)), [value, key])
+    const [value, setValue] = useState(getInitValue())
+
+    useEffect(handleEffectStorageChange, [initialState, isObject, sync, key])
+    useEffect(handleEffectItemUpdate, [key, value, isObject])
+
     return [value, setValue]
+
+    function getInitValue() {
+        if (localStorageValue === null) return initialState
+        else {
+            if (isObject) return JSON.parse(localStorageValue) as T
+            else return localStorageValue as T
+        }
+    }
+
+    function handleEffectItemUpdate() {
+        if (value !== undefined && value !== null) {
+            if (isObject) localStorage.setItem(key, JSON.stringify(value))
+            else localStorage.setItem(key, value.toString())
+        }
+    }
+
+    function handleEffectStorageChange() {
+        if (sync) {
+            window.addEventListener("storage", (e) => {
+                if (e.key === key) {
+                    if (e.newValue === null) setValue(initialState)
+                    else {
+                        if (isObject) setValue(JSON.parse(e.newValue) as T)
+                        else setValue(e.newValue as T)
+                    }
+                }
+            })
+        }
+    }
 }
--- web/src/provider/AuthProvider.tsx
+++ web/src/provider/AuthProvider.tsx
@@ -1,5 +1,7 @@
-import {createContext, ReactNode, useContext} from "react";
+import {createContext, ReactNode, useContext, useEffect} from "react";
 import {api} from "../app/api";
+import {useLocalStorageState} from "../hook/LocalStorage";
+import {useQueryClient} from "@tanstack/react-query";
 
 const AuthContext = createContext({
     setToken: (v: string) => {},
@@ -11,19 +13,29 @@ export function useAuth() {
 }
 
 export function AuthProvider(props: { children: ReactNode }) {
-    const token = localStorage.getItem("token")
-    if (token) setToken(token)
+    const [token, setToken] = useLocalStorageState("token", "", true);
+    const queryClient = useQueryClient();
+
+    useEffect(handleEffectTokenUpdate, [token, queryClient])
+
     return (
-        <AuthContext.Provider value={{setToken, logout}}>{props.children}</AuthContext.Provider>
+        <AuthContext.Provider value={{setToken: setTokenEncrypt, logout}}>
+            {props.children}
+        </AuthContext.Provider>
     )
 
-    function setToken(v: string){
-        localStorage.setItem("token", v)
-        api.defaults.headers.common["Authorization"] = `Bearer ${v}`
+    function handleEffectTokenUpdate() {
+        console.log(window.atob(token))
+        if (token !== "") api.defaults.headers.common["Authorization"] = `Bearer ${window.atob(token)}`
+        else delete api.defaults.headers.common["Authorization"]
+        queryClient.refetchQueries(["info"]).then()
+    }
+
+    function setTokenEncrypt(v: string){
+        setToken(window.btoa(v))
     }
 
     function logout(){
-        localStorage.removeItem("token")
-        delete api.defaults.headers.common["Authorization"]
+        setToken("")
     }
 }
