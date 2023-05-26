c40796a7b27e2f0f0ca7e2c68d50add960649cd7
Andrei Sergeev <aelsergeev@gmail.com>
2023-05-26 20:39:25 +0300

    fix unnecessary rerender that caused login window appearance each time after page reload

--- web/src/provider/AuthProvider.tsx
+++ web/src/provider/AuthProvider.tsx
@@ -1,4 +1,4 @@
-import {createContext, ReactNode, useContext, useEffect} from "react";
+import {createContext, ReactNode, useContext} from "react";
 import {api} from "../app/api";
 import {useLocalStorageState} from "../hook/LocalStorage";
 import {useQueryClient} from "@tanstack/react-query";
@@ -16,20 +16,15 @@ export function AuthProvider(props: { children: ReactNode }) {
     const [token, setToken] = useLocalStorageState("token", "", true);
     const queryClient = useQueryClient();
 
-    useEffect(handleEffectTokenUpdate, [token, queryClient])
+    if (token) api.defaults.headers.common["Authorization"] = `Bearer ${window.atob(token)}`
+    else delete api.defaults.headers.common["Authorization"]
+    queryClient.refetchQueries(["info"]).then()
 
     return (
         <AuthContext.Provider value={{setToken: setTokenEncrypt, logout}}>
             {props.children}
         </AuthContext.Provider>
     )
-
-    function handleEffectTokenUpdate() {
-        console.log(window.atob(token))
-        if (token !== "") api.defaults.headers.common["Authorization"] = `Bearer ${window.atob(token)}`
-        else delete api.defaults.headers.common["Authorization"]
-        queryClient.refetchQueries(["info"]).then()
-    }
 
     function setTokenEncrypt(v: string){
         setToken(window.btoa(v))
