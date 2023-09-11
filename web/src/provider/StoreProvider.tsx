3eec76fec4427eb9ccd41ad8968e26080d0f20c2
Andrei Sergeev <aelsergeev@gmail.com>
2023-09-11 12:14:01 +0300

    refactor store, wrap everything with callback

--- web/src/provider/StoreProvider.tsx
+++ web/src/provider/StoreProvider.tsx
@@ -1,4 +1,4 @@
-import {createContext, ReactNode, useContext} from "react";
+import {createContext, ReactNode, useCallback, useContext} from "react";
 import {useQueryClient} from "@tanstack/react-query";
 import {ActiveCluster, DetectionType} from "../type/cluster";
 import {InstanceWeb} from "../type/instance";
@@ -71,7 +71,23 @@ export function useStore() {
 
 export function StoreProvider(props: { children: ReactNode }) {
     const [state, setState] = useLocalStorageState("store", initialStore)
-    const queryClient = useQueryClient();
+    const queryClient = useQueryClient()
+
+    // setters
+    const setClusterCallback = useCallback(setCluster, [setState])
+    const setClusterInstanceCallback = useCallback(setClusterInstance, [setState])
+    const setClusterDetectionCallback = useCallback(setClusterDetection, [setState])
+    const setClusterTabCallback = useCallback(setClusterTab, [setState])
+    const setWarningsCallback = useCallback(setWarnings, [setState])
+    const setInstanceCallback = useCallback(setInstance, [setState])
+    const setTagsCallback = useCallback(setTags, [setState])
+    const toggleSettingsDialogCallback = useCallback(toggleSettingsDialog, [setState])
+    const clearCallback = useCallback(clear, [setState, queryClient])
+
+    // getters
+    const isClusterActiveMemo = useCallback(isClusterActive, [state.activeCluster])
+    const isClusterOverviewOpenMemo = useCallback(isClusterOverviewOpen, [state])
+    const isInstanceActiveMemo = useCallback(isInstanceActive, [state.activeInstance])
 
     const value = getStoreContext()
     return (
@@ -84,45 +100,66 @@ export function StoreProvider(props: { children: ReactNode }) {
         return {
             store: state,
 
-            setCluster: (cluster?: ActiveCluster) => {
-                setState(s => ({...s, activeCluster: cluster}))
-            },
-            setClusterInstance: (instance: InstanceWeb) => {
-                setState(s => {
-                    if (!s.activeCluster) return s
-                    return {...s, activeCluster: {...s.activeCluster, defaultInstance: instance, detection: "manual"}}
-                })
-            },
-            setClusterDetection: (detection: DetectionType) => {
-                setState(s => {
-                    if (!s.activeCluster) return s
-                    return {...s, activeCluster: {...s.activeCluster, detection}}
-                })
-            },
-            setClusterTab: (tab: number) => {
-                setState(s => ({...s, activeClusterTab: tab}))
-            },
-            setWarnings: (name: string, warning: boolean) => {
-                setState(s => ({...s, warnings: {...s.warnings, [name]: warning}}))
-            },
-            setInstance: (instance?: InstanceWeb) => {
-                setState(s => ({...s, activeInstance: instance}))
-            },
-            setTags: (tags: string[]) => {
-                setState(s => ({...s, activeTags: tags}))
-            },
-            toggleSettingsDialog: () => {
-                setState(s => ({...s, settings: !s.settings}))
-            },
-            clear: () => {
-                queryClient.clear()
-                setState(initialStore)
-            },
+            setCluster: setClusterCallback,
+            setClusterInstance: setClusterInstanceCallback,
+            setClusterDetection: setClusterDetectionCallback,
+            setClusterTab: setClusterTabCallback,
+            setWarnings: setWarningsCallback,
+            setInstance: setInstanceCallback,
+            setTags: setTagsCallback,
+            toggleSettingsDialog: toggleSettingsDialogCallback,
+            clear: clearCallback,
 
-
-            isClusterActive: (name: string) => name === state.activeCluster?.cluster.name,
-            isClusterOverviewOpen: () => !!state.activeCluster && state.activeClusterTab === 0,
-            isInstanceActive: (key: string) => state.activeInstance ? getDomain(state.activeInstance.sidecar) === key : false,
+            isClusterActive: isClusterActiveMemo,
+            isClusterOverviewOpen: isClusterOverviewOpenMemo,
+            isInstanceActive: isInstanceActiveMemo,
         }
+    }
+
+    // SETTERS
+    function setCluster(cluster?: ActiveCluster) {
+        setState(s => ({...s, activeCluster: cluster}))
+    }
+    function setClusterInstance(instance: InstanceWeb) {
+        setState(s => {
+            if (!s.activeCluster) return s
+            return {...s, activeCluster: {...s.activeCluster, defaultInstance: instance, detection: "manual"}}
+        })
+    }
+    function setClusterDetection(detection: DetectionType) {
+        setState(s => {
+            if (!s.activeCluster) return s
+            return {...s, activeCluster: {...s.activeCluster, detection}}
+        })
+    }
+    function setClusterTab(tab: number) {
+        setState(s => ({...s, activeClusterTab: tab}))
+    }
+    function setWarnings(name: string, warning: boolean) {
+        setState(s => ({...s, warnings: {...s.warnings, [name]: warning}}))
+    }
+    function setInstance(instance?: InstanceWeb) {
+        setState(s => ({...s, activeInstance: instance}))
+    }
+    function setTags(tags: string[]) {
+        setState(s => ({...s, activeTags: tags}))
+    }
+    function toggleSettingsDialog() {
+        setState(s => ({...s, settings: !s.settings}))
+    }
+    function clear() {
+        queryClient.clear()
+        setState(initialStore)
+    }
+
+    // GETTERS
+    function isClusterActive(name: string) {
+        return name === state.activeCluster?.cluster.name
+    }
+    function isClusterOverviewOpen() {
+        return !!state.activeCluster && state.activeClusterTab === 0
+    }
+    function isInstanceActive(key: string) {
+        return state.activeInstance ? getDomain(state.activeInstance.sidecar) === key : false
     }
 }
