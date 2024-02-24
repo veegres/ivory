f20797c19301372cee23daa636f76ac7186de2fd
Andrei Sergeev <aelsergeev@gmail.com>
2024-02-24 09:32:05 +0700

    fix history table rendering

--- web/src/component/shared/query/QueryBodyHistoryItem.tsx
+++ web/src/component/shared/query/QueryBodyHistoryItem.tsx
@@ -2,7 +2,7 @@ import {Accordion, AccordionDetails, AccordionSummary, Box} from "@mui/material"
 import {SimpleStickyTable} from "../../view/table/SimpleStickyTable";
 import {SxPropsMap} from "../../../type/common";
 import {QueryFields} from "../../../type/query";
-import {useState} from "react";
+import {useMemo, useState} from "react";
 import {KeyboardArrowDown} from "@mui/icons-material";
 
 const SX: SxPropsMap = {
@@ -13,20 +13,20 @@ const SX: SxPropsMap = {
     },
     details: {padding: "0 5px 5px"},
     info: {display: "flex", justifyContent: "space-between", fontSize: "12px", color: "text.secondary", gap: 1},
+    number: {color: "text.secondary", width: "20px", textAlign: "center"},
 }
 
 type Props = {
+    index: number,
     query: QueryFields,
 }
 
 export function QueryBodyHistoryItem(props: Props) {
-    const {query} = props
+    const {query, index} = props
     const [open, setOpen] = useState(false)
     const [show, setShow] = useState(false)
 
-    const columns = query.fields.map(field => (
-        {name: field.name, description: field.dataType}
-    ))
+    const columns = useMemo(handleMemoColumns, [query.fields])
 
     const endDate = new Date(query.endTime)
     const durationSec = (query.endTime - query.startTime) / 1000
@@ -41,6 +41,7 @@ export function QueryBodyHistoryItem(props: Props) {
             onMouseOver={() => setShow(true)}
         >
             <AccordionSummary sx={SX.summary} expandIcon={<KeyboardArrowDown sx={{fontSize: "20px"}}/>}>
+                <Box sx={SX.number}>{index+1}</Box>
                 <Box>{endDate.toLocaleString()}</Box>
                 {(open || show) && (
                     <Box sx={SX.info}>
@@ -54,4 +55,8 @@ export function QueryBodyHistoryItem(props: Props) {
             </AccordionDetails>
         </Accordion>
     )
+
+    function handleMemoColumns() {
+        return query.fields.map(field => ({name: field.name, description: field.dataType}))
+    }
 }
