03c37be8385013634c3d2990845066f0e3855e6d
Andrei Sergeev <aelsergeev@gmail.com>
2024-08-25 19:38:40 +0300

    - add hover for cell - refactor column dragger

--- web/src/component/view/table/VirtualizedTable.tsx
+++ web/src/component/view/table/VirtualizedTable.tsx
@@ -1,9 +1,10 @@
 import scroll from "../../../style/scroll.module.css";
 import {Box, CircularProgress, Tooltip} from "@mui/material";
 import {SxPropsMap} from "../../../type/general";
-import React, {Fragment, ReactNode, useCallback, useEffect, useRef, useState} from "react";
+import {Fragment, ReactNode, useRef} from "react";
 import {useVirtualizer} from "@tanstack/react-virtual";
 import {NoBox} from "../box/NoBox";
+import {useDragger} from "../../../hook/Dragger";
 
 const SX: SxPropsMap = {
     box: {position: "relative"},
@@ -14,12 +15,21 @@ const SX: SxPropsMap = {
         width: "inherit", height: "inherit", bgcolor: "rgba(31,44,126,0.05)",
     },
     cell: {
-        position: "absolute", top: 0, left: 0, padding: "0px 10px",
-        display: "flex", alignItems: "center",
-        borderBottom: 1, borderRight: 1,  bgcolor: "background.paper", borderColor: "divider",
+        position: "absolute", top: 0, left: 0, zIndex: 0, padding: "5px 10px",
+        bgcolor: "background.paper", borderBottom: 1, borderRight: 1, borderColor: "divider",
+        "&:hover": {
+            overflow: "auto", zIndex: 1,
+            width: "auto!important", height: "auto!important",
+            maxHeight: "150px", maxWidth: "400px",
+        },
+        "&:hover div": {
+            whiteSpace: "pre", overflow: "unset", textOverflow: "unset",
+        }
     },
+    noWrap: {whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis", width: "100%"},
+    preWrap: {whiteSpace: "pre-wrap", textWrap: "nowrap", overflow: "hidden", textOverflow: "ellipsis", width: "100%"},
     cellFixed: {
-        position: "absolute", top: 0, left: 0, padding: "0px 10px",
+        position: "absolute", top: 0, left: 0, padding: "5px 10px",
         display: "flex", alignItems: "center", justifyContent: "center",
         color: "text.disabled", bgcolor: "background.paper", borderColor: "divider",
     },
@@ -29,8 +39,6 @@ const SX: SxPropsMap = {
         display: "flex", alignItems: "center", justifyContent: "center",
         "&:hover:after": {content: `""`, width: "30%", height: "60%", bgcolor: "gray"},
     },
-    noWrap: {whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis", width: "100%"},
-    preWrap: {whiteSpace: "pre-wrap", textWrap: "nowrap", overflow: "hidden", textOverflow: "ellipsis", width: "100%"},
     headTitle: {display: "flex", fontFamily: "monospace", gap: "4px"},
     empty: {padding: "10px"},
 }
@@ -48,38 +56,40 @@ export function VirtualizedTable(props: Props) {
     const {columns, rows, renderRowCell} = props
     const {fetching = false, width = "100%", height = "391px"} = props
     const parentRef = useRef<Element>(null)
-    const [columnDrag, setColumnDrag] = useState({index: -1, size: 0, ps: 0})
 
-    const rowHeight = 32
-    const rowHeightPx = `${rowHeight}px`
+    const actionColumn = renderRowCell !== undefined
+    const columnCount = actionColumn ? columns.length + 1 : columns.length
+    const scrollOffset = 6
+
+    const cellWidthSticky = 50
+    const cellHeightSticky = 32
+    const cellWidthStickyPx = `${cellWidthSticky}px`
+    const cellHeightStickyPx = `${cellHeightSticky}px`
+
+    // NOTE: calculate the cell width, the component should be rendered from scratch to change the size,
+    //  this is how useVirtualizer work (-1 is needed to always choose 100 if width is unknown)
+    const cellWidth = Math.max((Math.round((parentRef.current?.clientWidth ?? -1) - cellWidthSticky - scrollOffset) / columnCount), 100)
+    const cellHeight = 29
+
     const rowVirtualizer = useVirtualizer({
         count: rows.length,
         getScrollElement: () => parentRef.current,
-        estimateSize: () => rowHeight,
-        scrollPaddingStart: rowHeight,
+        estimateSize: () => cellHeight,
+        scrollPaddingStart: cellHeightSticky,
         overscan: 3,
     })
-
-    const scrollOffset = 6
-    const columnCount = renderRowCell !== undefined ? columns.length + 1 : columns.length
-    const columnWidthId = 50
-    // NOTE: calculate the cell width, the component should be rendered from scratch to change the size,
-    //  this is how useVirtualizer work (-1 is needed to always choose 100 if width is unknown)
-    const columnWidth = Math.max((Math.round((parentRef.current?.clientWidth ?? -1) - columnWidthId - scrollOffset) / columnCount), 100)
-    const columnWidthPx = `${columnWidthId}px`
     const columnVirtualizer = useVirtualizer({
         horizontal: true,
         count: columnCount,
         getScrollElement: () => parentRef.current,
-        estimateSize: () => columnWidth,
-        scrollPaddingStart: columnWidthId,
+        estimateSize: () => cellWidth,
+        scrollPaddingStart: cellWidthSticky,
         overscan: 3,
+        // NOTE: this helps to use cellWidth, because in first render when we don't have ref, it
+        //  renders incorrect size
         enabled: parentRef.current !== null,
     })
-
-    const callbackMouseMove = useCallback(handleMouseMove, [columnDrag.index, columnDrag.ps, columnDrag.size, columnVirtualizer])
-    const callbackMouseUp = useCallback(handleMouseUp, [])
-    useEffect(handleEffectDragging, [callbackMouseMove, callbackMouseUp]);
+    const columnDragger = useDragger(columnVirtualizer.resizeItem)
 
     const bodyWidthPx = `${columnVirtualizer.getTotalSize()}px`
     const bodyHeightPx = `${rowVirtualizer.getTotalSize()}px`
@@ -87,7 +97,7 @@ export function VirtualizedTable(props: Props) {
     return (
         <Box sx={SX.box} width={width} height={height}>
             {renderLoader()}
-            {columns.length ? renderTable() : renderEmpty()}
+            {columns.length > 0 ? renderTable() : renderEmpty()}
         </Box>
     )
 
@@ -95,7 +105,7 @@ export function VirtualizedTable(props: Props) {
         if (!fetching) return
         return (
             <Box sx={SX.loader}>
-                <CircularProgress />
+                <CircularProgress/>
             </Box>
         )
     }
@@ -116,8 +126,8 @@ export function VirtualizedTable(props: Props) {
                 sx={SX.body}
                 padding={`0px ${scrollOffset}px ${scrollOffset}px 0px`}
                 display={"grid"}
-                gridTemplateColumns={`${columnWidthPx} ${bodyWidthPx}`}
-                gridTemplateRows={`${rowHeightPx} ${bodyHeightPx}`}
+                gridTemplateColumns={`${cellWidthStickyPx} ${bodyWidthPx}`}
+                gridTemplateRows={`${cellHeightStickyPx} ${bodyHeightPx}`}
                 className={scroll.small}
             >
                 <Box position={"sticky"} zIndex={3} top={0} left={0}>
@@ -138,38 +148,38 @@ export function VirtualizedTable(props: Props) {
 
     function renderCorner() {
         return (
-            <Box sx={SX.cellFixed} border={1} borderBottom={2} borderRight={2} style={{width: columnWidthPx, height: rowHeightPx}}/>
+            <Box sx={SX.cellFixed} border={1} borderBottom={2} borderRight={2}
+                 style={{width: cellWidthStickyPx, height: cellHeightStickyPx}}/>
         )
     }
 
     function renderHead() {
-        if (!columns) return
-        const heightPx = `${rowHeight}px`
         const dragWidth = scrollOffset * 2
         const dragWidthPx = `${dragWidth}px`
         const dragOffset = scrollOffset + 1
-
+        const columnsWithAction = actionColumn ? [...columns, {name: "", description: ""}] : columns
+        // TODO in initial render with empty body it doesn't display head
         return columnVirtualizer.getVirtualItems().map((virtualColumn) => {
-            const column = columns.length <= virtualColumn.index ? {name: "", description: ""} : columns[virtualColumn.index]
+            const column = columnsWithAction[virtualColumn.index]
             const width = `${virtualColumn.size}px`
             const transform = `translateX(${virtualColumn.start}px)`
             const dragTransform = `translateX(${virtualColumn.start + virtualColumn.size - dragOffset}px)`
             return (
                 <Fragment key={virtualColumn.key}>
-                    <Box sx={SX.cellFixed} borderRight={1} borderTop={1} borderBottom={2} style={{width, height: heightPx, transform}}>
-                        <Tooltip title={renderHeadTitle(column.name, column.description)} placement={"top"} arrow={true}>
+                    <Box sx={SX.cellFixed} borderRight={1} borderTop={1} borderBottom={2}
+                         style={{width, height: cellHeightStickyPx, transform}}>
+                        <Tooltip title={renderHeadTitle(column.name, column.description)} placement={"top"}>
                             <Box sx={SX.noWrap}>{column.name}</Box>
                         </Tooltip>
                     </Box>
                     <Box
                         sx={SX.columnSeparator}
-                        style={{height: heightPx, width: dragWidthPx, transform: dragTransform}}
-                        onMouseDown={(e) => handleMouseDown(e, virtualColumn.index, virtualColumn.size)}
+                        style={{width: dragWidthPx, height: cellHeightStickyPx, transform: dragTransform}}
+                        onMouseDown={(e) => columnDragger.onMouseDown(e, virtualColumn.index, virtualColumn.size)}
                     />
                 </Fragment>
-
-            )}
-        )
+            )
+        })
     }
 
     function renderHeadTitle(name: string, description?: string) {
@@ -182,13 +192,14 @@ export function VirtualizedTable(props: Props) {
     }
 
     function renderIndex() {
-        const width = `${columnWidthId}px`
-        const height = `${rowHeight}px`
+        const width = `${cellWidthSticky}px`
+        const height = `${cellHeight}px`
 
         return rowVirtualizer.getVirtualItems().map((virtualRow) => {
             const transform = `translateY(${virtualRow.start}px)`
             return (
-                <Box key={virtualRow.key} sx={SX.cellFixed} borderLeft={1} borderBottom={1} borderRight={2} style={{width, height, transform}}>
+                <Box key={virtualRow.key} sx={SX.cellFixed} borderLeft={1} borderBottom={1} borderRight={2}
+                     style={{width, height, transform}}>
                     {virtualRow.index + 1}
                 </Box>
             )
@@ -198,7 +209,6 @@ export function VirtualizedTable(props: Props) {
     function renderBody() {
         if (!columns) return
         const wrap = columnCount === 1 ? SX.preWrap : SX.noWrap
-
         return rowVirtualizer.getVirtualItems().map((virtualRow) => {
             const row = rows[virtualRow.index]
             return (
@@ -208,9 +218,10 @@ export function VirtualizedTable(props: Props) {
                         const height = `${virtualRow.size}px`
                         const width = `${virtualColumn.size}px`
                         const transform = `translateX(${virtualColumn.start}px) translateY(${virtualRow.start}px)`
+                        const value = parseColumn(row)
                         return (
-                            <Box key={virtualColumn.key} sx={SX.cell} style={{width, height, transform}}>
-                                <Box sx={wrap}>{parseColumn(row)}</Box>
+                            <Box key={virtualColumn.key} sx={SX.cell} className={scroll.tiny} style={{minWidth: width, minHeight: height, width, height, transform}}>
+                                <Box sx={wrap}>{value}</Box>
                             </Box>
                         )
                     })}
@@ -225,35 +236,5 @@ export function VirtualizedTable(props: Props) {
         if (typeof column === 'object') return () => JSON.stringify(column)
         else if (typeof column === "boolean") return () => Boolean(column).toString()
         else return () => column
-    }
-
-    function handleEffectDragging() {
-        document.addEventListener("mousemove", callbackMouseMove)
-        document.addEventListener("mouseup", callbackMouseUp)
-
-        return () => {
-            document.removeEventListener("mousemove", callbackMouseMove)
-            document.addEventListener("mouseup", callbackMouseUp)
-        }
-    }
-
-    function handleMouseMove(e: MouseEvent) {
-        if (columnDrag.index != -1) {
-            const offset = e.pageX - columnDrag.ps
-            const width = columnDrag.size + offset
-            if (width > 50) columnVirtualizer.resizeItem(columnDrag.index, width)
-        }
-    }
-
-    function handleMouseUp() {
-        document.body.style.removeProperty("cursor")
-        setColumnDrag({index: -1, size: 0, ps: 0})
-    }
-
-    function handleMouseDown(e: React.MouseEvent, index: number, size: number) {
-        e.preventDefault()
-        e.stopPropagation()
-        document.body.style.cursor = "col-resize"
-        setColumnDrag({index, size, ps: e.pageX})
     }
 }
