4d11dfb4eb792de85fce7cc8f818596dd62811ca
Andrei Sergeev <aelsergeev@gmail.com>
2024-08-28 17:28:52 +0300

    improve instance info

--- web/src/component/pages/cluster/instance/InstanceInfo.tsx
+++ web/src/component/pages/cluster/instance/InstanceInfo.tsx
@@ -1,13 +1,15 @@
-import {Box, ToggleButton, ToggleButtonGroup} from "@mui/material";
+import {Box, Paper, ToggleButton, ToggleButtonGroup} from "@mui/material";
 import {InstanceInfoStatus} from "./InstanceInfoStatus";
 import {InstanceInfoTable} from "./InstanceInfoTable";
 import {SxPropsMap} from "../../../../type/general";
 import {InstanceTabType, InstanceWeb} from "../../../../type/instance";
 import {QueryActivity} from "../../../shared/query/QueryActivity";
 import {QueryConnection} from "../../../../type/query";
+import {SxPropsFormatter} from "../../../../app/utils";
 
 const SX: SxPropsMap = {
-    info: {display: "flex", flexDirection: "column", gap: 2, margin: "5px 0"},
+    info: {display: "flex", flexDirection: "column", gap: 1, margin: "5px 0"},
+    paper: {padding: "5px", bgcolor: SxPropsFormatter.style.bgImageSelected},
 }
 
 type Props = {
@@ -23,12 +25,20 @@ export function InstanceInfo(props: Props) {
     return (
         <Box sx={SX.info}>
             <ToggleButtonGroup size={"small"} color={"secondary"} fullWidth value={tab}>
-                <ToggleButton value={InstanceTabType.CHART} onClick={() => onTab(InstanceTabType.CHART)}>Charts</ToggleButton>
-                <ToggleButton value={InstanceTabType.QUERY} onClick={() => onTab(InstanceTabType.QUERY)}>Queries</ToggleButton>
+                <ToggleButton value={InstanceTabType.CHART} onClick={() => onTab(InstanceTabType.CHART)}>
+                    Charts
+                </ToggleButton>
+                <ToggleButton value={InstanceTabType.QUERY} onClick={() => onTab(InstanceTabType.QUERY)}>
+                    Queries
+                </ToggleButton>
             </ToggleButtonGroup>
             <InstanceInfoStatus role={instance.role}/>
-            <InstanceInfoTable instance={instance}/>
-            <QueryActivity connection={connection}/>
+            <Paper sx={SX.paper} variant={"outlined"}>
+                <InstanceInfoTable instance={instance}/>
+            </Paper>
+            <Paper sx={SX.paper} variant={"outlined"}>
+                <QueryActivity connection={connection}/>
+            </Paper>
         </Box>
     )
 }
