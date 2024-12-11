5f58e76f76ff1d0fbda6cb620cdf894823f82f98
Andrei Sergeev <aelsergeev@gmail.com>
2024-12-11 21:46:18 +0300

    add refresh button for active queries

--- web/src/component/shared/query/QueryActivity.tsx
+++ web/src/component/shared/query/QueryActivity.tsx
@@ -3,10 +3,12 @@ import {useRouterActivity} from "../../../router/query";
 import {QueryTable} from "./QueryTable";
 import {Box} from "@mui/material";
 import {SxPropsMap} from "../../../type/general";
+import {RefreshIconButton} from "../../view/button/IconButtons";
 
 const SX: SxPropsMap = {
     box: {display: "flex", flexDirection: "column", gap: 1, width: "320px", minHeight: "250px"},
-    label: {textAlign: "center", fontSize: "15px", fontWeight: "bold", color: "text.secondary"},
+    button: {position: "absolute", right: 10, top: 0, bottom: 0, display: "flex", alignItems: "center"},
+    label: {position: "relative", textAlign: "center", fontSize: "15px", fontWeight: "bold", color: "text.secondary"},
     help: {fontSize: "9px", fontWeight: "normal", color: "text.disabled"},
 }
 
@@ -16,12 +18,18 @@ type Props = {
 
 export function QueryActivity(props: Props) {
     const {connection} = props
-    const {data} = useRouterActivity(connection)
+    const {data, refetch, isFetching} = useRouterActivity(connection)
     return (
         <Box sx={SX.box}>
             <Box sx={SX.label}>
-                <Box>User Active Queries</Box>
+                <Box>Your Active Queries</Box>
                 <Box sx={SX.help}>[ hold shift for horizontal scrolling ]</Box>
+                <Box sx={SX.button}>
+                    <RefreshIconButton
+                        disabled={isFetching}
+                        onClick={() => refetch()}
+                    />
+                </Box>
             </Box>
             <QueryTable
                 connection={connection}
