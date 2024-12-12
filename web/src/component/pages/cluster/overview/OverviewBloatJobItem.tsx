5ac5534b2923dbd499ad1600b51df1a0c2ee1487
Andrei Sergeev <aelsergeev@gmail.com>
2024-12-12 15:26:54 +0300

    remove deprecated Grid component

--- web/src/component/pages/cluster/overview/OverviewBloatJobItem.tsx
+++ web/src/component/pages/cluster/overview/OverviewBloatJobItem.tsx
@@ -1,4 +1,4 @@
-import {Box, CircularProgress, Divider, Grid, IconButton, Paper, Tooltip} from "@mui/material";
+import {Box, CircularProgress, Divider, IconButton, Paper, Tooltip} from "@mui/material";
 import {cloneElement, ReactElement, useState} from "react";
 import {OpenIcon} from "../../../view/icon/OpenIcon";
 import {Clear, Stop} from "@mui/icons-material";
@@ -18,14 +18,15 @@ const SX: SxPropsMap = {
     console: {fontSize: "13px", width: "100%", padding: "8px 15px"},
     row: {"&:hover": {color: "primary.main"}},
     emptyLine: {textAlign: "center", textTransform: "uppercase"},
-    header: {fontWeight: "bold", cursor: "pointer"},
+    header: {display: "flex", flexDirection: "column", fontWeight: "bold", cursor: "pointer"},
+    headerLine: {display: "flex", justifyContent: "space-between", flexWrap: "nowrap"},
     loader: {margin: "10px 0 5px"},
     divider: {margin: "5px 0"},
     logs: {colorScheme: "dark"},
     button: {padding: "1px"},
     tooltipBox: {marginLeft: "4px", width: "25px", display: "flex", alignItems: "center", justifyContent: "center"},
-    separator: {marginLeft: "10px"},
-    credential: {color: "text.disabled", fontWeight: 500}
+    separator: {display: "flex", alignItems: "start", marginLeft: "10px"},
+    credential: {display: "inline", color: "text.secondary", marginLeft: "5px"},
 }
 
 type Props = {
@@ -44,26 +45,30 @@ export function OverviewBloatJobItem(props: Props) {
 
     return (
         <Paper sx={SX.paper} variant={"outlined"}>
-            <Grid container sx={SX.header} onClick={() => setOpen(!open)} className={select.none}>
-                <Grid item container justifyContent={"space-between"} flexWrap={"nowrap"}>
-                    <Grid item container gap={1}>
-                        <Box>Command</Box>
-                        <Tooltip title={"Postgres Password ID"} placement={"right"}>
-                            <Box sx={SX.credential}>{shortUuid(credentialId)}</Box>
-                        </Tooltip>
-                    </Grid>
-                    <Grid item container xs={"auto"} sx={SX.separator}>
+            <Box sx={SX.header} onClick={() => setOpen(!open)} className={select.none}>
+                <Box sx={SX.headerLine}>
+                    <Box>Command</Box>
+                    <Box sx={SX.separator}>
                         <Box sx={{color: status.color}}>{status.name}</Box>
                         {status.active ?
                             renderJobButton("Stop", <Stop/>, () => stopJob.mutate(uuid), stopJob.isPending) :
                             renderJobButton("Delete", <Clear/>, () => deleteJob.mutate(uuid), deleteJob.isPending)
                         }
-                    </Grid>
-                </Grid>
-                <Grid item container justifyContent={"space-between"} flexWrap={"nowrap"}>
-                    <Grid item>{command}</Grid>
-                    <Grid item container xs={"auto"} sx={SX.separator}>
-                        <Tooltip title={uuid}><Box>{shortUuid(uuid)}</Box></Tooltip>
+                    </Box>
+                </Box>
+                <Box sx={SX.headerLine}>
+                    <Box>
+                        {command}
+                        <Tooltip title={renderPasswordTooltip()} placement={"top"}>
+                            <Box sx={SX.credential}>
+                                --username {shortUuid(credentialId)} --password {shortUuid(credentialId)}
+                            </Box>
+                        </Tooltip>
+                    </Box>
+                    <Box sx={SX.separator}>
+                        <Tooltip title={`Job ID: ${uuid}`}>
+                            <Box>{shortUuid(uuid)}</Box>
+                        </Tooltip>
                         <Tooltip title={"Open"}>
                             <Box sx={SX.tooltipBox}>
                                 <IconButton sx={SX.button} size={"small"}>
@@ -71,12 +76,21 @@ export function OverviewBloatJobItem(props: Props) {
                                 </IconButton>
                             </Box>
                         </Tooltip>
-                    </Grid>
-                </Grid>
-            </Grid>
+                    </Box>
+                </Box>
+            </Box>
             {renderBody()}
         </Paper>
     )
+
+    function renderPasswordTooltip() {
+        return (
+            <Box>
+                <Box><b>Ivory Credentials Id</b></Box>
+                <Box>[ provided only for transparency, you need to provide real password and username ]</Box>
+            </Box>
+        )
+    }
 
     function renderBody() {
         if (!open) return
