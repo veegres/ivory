60cc761e2ae2e21fc07dea31bfb82db2674e5350
Andrei Sergeev <aelsergeev@gmail.com>
2024-12-12 15:26:54 +0300

    remove deprecated Grid component

--- web/src/component/pages/cluster/instance/InstanceInfoStatus.tsx
+++ web/src/component/pages/cluster/instance/InstanceInfoStatus.tsx
@@ -1,10 +1,11 @@
-import {Grid, Skeleton} from "@mui/material";
+import {Box, Skeleton} from "@mui/material";
 import {InstanceColor} from "../../../../app/utils";
 import {SxPropsMap} from "../../../../type/general";
 import {Role} from "../../../../type/instance";
 
 const SX: SxPropsMap = {
     instanceStatusBlock: {
+        display: "flex", alignItems: "center", justifyContent: "center",
         height: "120px", minWidth: "250px", borderRadius: "4px",
         color: "white", fontSize: "24px", fontWeight: 900,
     },
@@ -17,12 +18,12 @@ type Props = {
 
 export function InstanceInfoStatus(props: Props) {
     const {role, loading} = props
-    if (loading) return <Skeleton variant="rectangular" sx={SX.instanceStatusBlock}/>
+    if (loading) return <Skeleton variant={"rectangular"} sx={SX.instanceStatusBlock}/>
     const background = role && InstanceColor[role]
 
     return (
-        <Grid container alignContent="center" justifyContent="center" sx={{...SX.instanceStatusBlock, background}}>
-            <Grid item>{role?.toUpperCase() ?? "unknown"}</Grid>
-        </Grid>
+        <Box sx={{...SX.instanceStatusBlock, background}}>
+            {role?.toUpperCase() ?? "unknown"}
+        </Box>
     )
 }
