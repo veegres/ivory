c3c8973f0d5cf27154cf263d48427f2d0a4bc692
Andrei Sergeev <aelsergeev@gmail.com>
2025-02-03 21:08:09 +0300

    improve layout

--- web/src/component/pages/cluster/ClusterBody.tsx
+++ web/src/component/pages/cluster/ClusterBody.tsx
@@ -1,5 +1,5 @@
 import {Menu} from "../../settings/menu/Menu";
-import {Stack} from "@mui/material";
+import {Box, Stack} from "@mui/material";
 import {List as ClusterList} from "./list/List";
 import {Overview as ClusterOverview} from "./overview/Overview";
 import {Instance as ClusterInstance} from "./instance/Instance";
@@ -9,16 +9,15 @@ const SX: SxPropsMap = {
     stack: {width: "100%", height: "100%", gap: 4}
 }
 
-
 export function ClusterBody() {
     return (
-        <>
+        <Box>
             <Menu/>
             <Stack sx={SX.stack}>
                 <ClusterList/>
                 <ClusterOverview/>
                 <ClusterInstance/>
             </Stack>
-        </>
+        </Box>
     )
 }
