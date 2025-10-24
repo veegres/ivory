97b5b487ab405394400d9b0f73b96b57cb13ca68
Andrei Sergeev <aelsergeev@gmail.com>
2025-10-24 19:27:51 +0300

    improve auth UI

--- web/src/component/pages/login/LoginBody.tsx
+++ web/src/component/pages/login/LoginBody.tsx
@@ -1,5 +1,5 @@
 import {PageStartupBox} from "../../view/box/PageStartupBox";
-import {Alert, Button} from "@mui/material";
+import {Alert, Button, Divider, ToggleButton, ToggleButtonGroup} from "@mui/material";
 import {KeyEnterInput} from "../../view/input/KeyEnterInput";
 import {useState} from "react";
 import {useRouterLogin, useRouterLogout} from "../../../api/auth/hook";
@@ -8,6 +8,8 @@ import {AuthType} from "../../../api/auth/type";
 
 const SX: SxPropsMap = {
     alert: {width: "100%", padding: "0 20px", justifyContent: "center"},
+    label: {color: "text.secondary", fontWeight: "bold", fontSize: "25px"},
+    types: {display: "flex", gap: 1, width: "100%", flexDirection: "column"},
 }
 
 type Props = {
@@ -17,6 +19,7 @@ type Props = {
 
 export function LoginBody(props: Props) {
     const {supported, error} = props
+    const [auth, setAuth] = useState<AuthType>(supported[0] ?? AuthType.BASIC)
     const [username, setUsername] = useState("")
     const [password, setPass] = useState("")
 
@@ -24,14 +27,17 @@ export function LoginBody(props: Props) {
     const logoutRouter = useRouterLogout()
 
     return (
-        <PageStartupBox header={"Authentication"} renderFooter={renderButtons()}>
+        <PageStartupBox header={"Authentication"} renderFooter={renderFooter()} position={"start"}>
+            {renderButtons()}
+            <Divider/>
             {error && <Alert sx={SX.alert} severity={"warning"} icon={false}>{error}</Alert>}
+            <Divider/>
             {renderCreds()}
         </PageStartupBox>
     )
 
     function renderCreds() {
-        if (![AuthType.BASIC, AuthType.LDAP].some(r => supported.includes(r))) return null
+        if ([AuthType.OIDC].includes(auth)) return null
 
         return (
             <>
@@ -46,12 +52,28 @@ export function LoginBody(props: Props) {
         )
     }
 
-    function renderButtons() {
+    function renderFooter() {
         if (!supported.length) return <Button onClick={handleLogout}>Sign out</Button>
 
-        return supported.map(type => (
-            <Button key={type} onClick={() => handleLogin(type)} loading={login.isPending}>Sign in with {AuthType[type]}</Button>
-        ))
+        return (
+            <Button onClick={() => handleLogin(auth)} loading={login.isPending} fullWidth={true}>
+                SIGN IN
+            </Button>
+        )
+    }
+
+    function renderButtons() {
+        if (supported.length < 2) return null
+
+        return (
+            <ToggleButtonGroup value={auth} exclusive={true} fullWidth={true} size={"small"}>
+                {supported.map(type => (
+                    <ToggleButton value={type} key={type} onClick={() => setAuth(type)}>
+                        {AuthType[type]}
+                    </ToggleButton>
+                ))}
+            </ToggleButtonGroup>
+        )
     }
 
     function handleLogout() {
