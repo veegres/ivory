dc93b401494434204c9ca14819d391bf8769ba4f
Andrei Sergeev <aelsergeev@gmail.com>
2025-10-24 14:45:18 +0300

    add test connection button to UI

--- web/src/component/pages/config/auth/ConfigAuthOidc.tsx
+++ web/src/component/pages/config/auth/ConfigAuthOidc.tsx
@@ -1,8 +1,10 @@
-import {Box} from "@mui/material";
+import {Box, Button} from "@mui/material";
 import {SxPropsMap} from "../../../../app/type";
 import {KeyEnterInput} from "../../../view/input/KeyEnterInput";
 import {OidcConfig} from "../../../../api/config/type";
 import {ChangeEvent} from "react";
+import {useRouterConnect} from "../../../../api/auth/hook";
+import {AuthType} from "../../../../api/auth/type";
 
 const SX: SxPropsMap = {
     box: {display: "flex", flexDirection: "column", gap: 1},
@@ -15,16 +17,9 @@ type Props = {
 
 export function ConfigAuthOidc(props: Props) {
     const {config, onChange} = props
+    const connect = useRouterConnect()
     return (
         <Box sx={SX.box}>
-            <KeyEnterInput
-                label={"Issuer URL"}
-                value={config.issuerUrl}
-                helperText={"Specify the complete HTTP url, including the protocol prefix http:// or https://"}
-                onChange={handleConfigChange("issuerUrl")}
-            />
-            <KeyEnterInput label={"Client ID"} value={config.clientId} onChange={handleConfigChange("clientId")}/>
-            <KeyEnterInput label={"Client secret"} value={config.clientSecret} onChange={handleConfigChange("clientSecret")} hidden/>
             <KeyEnterInput
                 label={"Redirect URL"}
                 value={config.redirectUrl}
@@ -33,6 +28,20 @@ export function ConfigAuthOidc(props: Props) {
                 disabled={true}
                 onChange={handleConfigChange("redirectUrl")}
             />
+            <KeyEnterInput
+                label={"Issuer URL"}
+                value={config.issuerUrl}
+                helperText={"Specify the complete HTTP url, including the protocol prefix http:// or https://"}
+                onChange={handleConfigChange("issuerUrl")}
+            />
+            <KeyEnterInput label={"Client ID"} value={config.clientId} onChange={handleConfigChange("clientId")}/>
+            <KeyEnterInput label={"Client secret"} value={config.clientSecret} onChange={handleConfigChange("clientSecret")} hidden/>
+            <Button
+                color={"success"}
+                loading={connect.isPending}
+                onClick={() => connect.mutate({type: AuthType.OIDC, config})}>
+                Test Connection
+            </Button>
         </Box>
     )
 
