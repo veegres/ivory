7c3eaef4f8e8f68a3bdd717de08de1f6bc0e45a0
Andrei Sergeev <aelsergeev@gmail.com>
2025-12-10 18:14:14 +0300

    improve tags error and load handling

--- web/src/component/pages/cluster/list/List.tsx
+++ web/src/component/pages/cluster/list/List.tsx
@@ -1,24 +1,38 @@
 import {useEffect} from "react"
 
 import {useRouterClusterList} from "../../../../api/cluster/hook"
+import {useRouterTagList} from "../../../../api/tag/hook"
 import {useStore} from "../../../../provider/StoreProvider"
+import {ErrorSmart} from "../../../view/box/ErrorSmart";
 import {PageMainBox} from "../../../view/box/PageMainBox"
 import {ListTable} from "./ListTable"
 import {ListTags} from "./ListTags"
 
 export function List() {
     const activeTags = useStore(s => s.activeTags)
-    const query = useRouterClusterList(activeTags)
-    const {data, isPending, isFetching, error} = query
+    const clusters = useRouterClusterList(activeTags)
+    const tags = useRouterTagList()
 
-    // NOTE: we don't use queryKey to update it, because it will create separate request and cause new fetching
+    // NOTE: we don't use queryKey to update it, because it will create a separate request and cause new fetching
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    useEffect(() => { query.refetch().then() }, [activeTags])
+    useEffect(() => { clusters.refetch().then() }, [activeTags])
 
     return (
         <PageMainBox withMarginTop={"40px"}>
-            <ListTags/>
-            <ListTable map={data} error={error} fetching={isFetching} pending={isPending}/>
+            {renderBody()}
         </PageMainBox>
     )
+
+    function renderBody() {
+        if (clusters.error) return <ErrorSmart error={clusters.error}/>
+        if (tags.error) return <ErrorSmart error={tags.error}/>
+
+        return (
+            <>
+                <ListTags tags={tags.data ?? []}/>
+                <ListTable map={clusters.data} fetching={clusters.isFetching} pending={clusters.isPending || tags.isPending}/>
+            </>
+        )
+
+    }
 }
