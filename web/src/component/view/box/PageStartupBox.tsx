355530fbfa53cd17a85695a4be19bfa9a500a1b8
Andrei Sergeev <aelsergeev@gmail.com>
2025-12-14 13:14:04 +0300

    customize padding for permissions view

--- web/src/component/view/box/PageStartupBox.tsx
+++ web/src/component/view/box/PageStartupBox.tsx
@@ -10,7 +10,7 @@ import {LinearProgressStateful} from "../progress/LinearProgressStateful"
 const SX: SxPropsMap = {
     box: {
         display: "flex", flexDirection: "column", alignItems: "center",
-        width: "30%", minWidth: "500px", gap: 2, padding: "150px 0px",
+        width: "30%", minWidth: "500px", gap: 2
     },
     header: {fontSize: "35px", fontWeight: 900, fontFamily: "monospace", margin: "20px 0", cursor: "pointer"},
     buttons: {display: "flex", gap: 2, width: "100%", justifyContent: "center", alignItems: "center"},
@@ -21,16 +21,17 @@ type Props = {
     header: string,
     renderFooter: ReactNode,
     position?: "center" | "start" | "end",
+    padding?: string,
 }
 
 export function PageStartupBox(props: Props) {
-    const {children, header, renderFooter, position = "center"} = props
+    const {children, header, renderFooter, position = "center", padding = "150px 0px"} = props
     const [animal, setAnimal] = useState(randomUnicodeAnimal())
 
     const info = useRouterInfo()
 
     return (
-        <Box sx={SX.box} justifyContent={position}>
+        <Box sx={SX.box} justifyContent={position} padding={padding}>
             <Box sx={SX.header} className={select.none} onClick={() => setAnimal(randomUnicodeAnimal())}>
                 {header} {animal}
             </Box>
