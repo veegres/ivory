4ab72daa69aa8cd8c139f35a7c92cfa42d1f6c2b
Andrei Sergeev <aelsergeev@gmail.com>
2026-05-20 12:14:09 +0300

    add useStream hook

--- app/features/bloat/job/type.ts
+++ app/features/bloat/job/type.ts
@@ -14,6 +14,7 @@ export enum EventType {
     STATUS = "status",
     LOG = "log",
     STREAM = "stream",
+    BROWSER = "browser",
 }
 
 export enum EventStreamType {
---
+++ app/shared/hook/Stream.ts
@@ -0,0 +1,39 @@
+import {useCallback, useEffect, useState} from "react"
+
+import {EventType} from "../../features/bloat/job/type"
+
+type MessageEvent = {
+    type: EventType,
+    message: string,
+}
+
+export function useStream(url: string) {
+    const [loading, setLoading] = useState(false)
+    const [response, setResponse] = useState<MessageEvent[]>([])
+
+    const push = useCallback((type: EventType, message: string) =>
+        setResponse((prev) => [...prev, {type, message}]), [])
+
+    useEffect(() => {
+        if (!url) return
+        const es = new EventSource(url)
+        es.onopen = () => {
+            setLoading(true)
+            push(EventType.BROWSER, "streaming open: New connection was established")
+        }
+        Object.values(EventType).forEach((type) => {
+            es.addEventListener(type, (e) => push(type, e.data))
+        })
+        es.onerror = () => {
+            setLoading(false)
+            push(EventType.BROWSER, "streaming error: Trying to reestablish connection")
+        }
+        return () => {
+            es.close()
+            setLoading(false)
+            setResponse([])
+        }
+    }, [push, url])
+
+    return {loading, response}
+}
